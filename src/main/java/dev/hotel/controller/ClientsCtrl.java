de463e572b436adf58b81d293e52792090a31461
DeJulien <57177431+DeJulien@users.noreply.github.com>
2020-01-27 17:28:53 +0100

    tp 1 maj clean

--- src/main/java/dev/hotel/controller/ClientsCtrl.java
+++ src/main/java/dev/hotel/controller/ClientsCtrl.java
@@ -1,62 +1,58 @@
 package dev.hotel.controller;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.UUID;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import javax.persistence.EntityExistsException;
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.hotel.entite.Client;
-import dev.hotel.repository.ClientRepository;
+import dev.hotel.service.ClientService;
 
 @RestController
-
+@RequestMapping("clients")
 public class ClientsCtrl {
-	private static final Logger LOG = LoggerFactory.getLogger(ClientsCtrl.class);
-	private ClientRepository clientRepository;
 
-	public ClientsCtrl(ClientRepository clientRepository) {
+	private ClientService clientService;
+
+	public ClientsCtrl(ClientService clientService) {
 		super();
-		this.clientRepository = clientRepository;
+		this.clientService = clientService;
 	}
 
-	@RequestMapping(method = RequestMethod.GET, path = "clients/lister")
-	public List<Client> retournePlusieursEmployes() {
-		List<Client> listClient = clientRepository.findAll();
-		return listClient;
-
+	@GetMapping
+	public List<Client> listerClients() {
+		return this.clientService.listerClients();
 	}
 
-	@RequestMapping(method = RequestMethod.GET, path = "clients")
-	public Client[] rechercheNom(@RequestParam("nom") String nomRequeteHttp) {
-		return this.clientRepository.findByNom("Pierre");
-
+	@GetMapping(params = "nom")
+	public List<Client> rechercherParNom(@RequestParam("nom") String nomRequete) {
+		return this.clientService.findByNom(nomRequete);
 	}
 
-	@RequestMapping(method = RequestMethod.POST, path = "clients")
-	// @ResponseBody
-	public ResponseEntity<String> post(@RequestBody Client clientRecu) {
-
-		Optional<Client> clients = this.clientRepository.findByNomAndPrenoms(clientRecu.getNom().toUpperCase(),
-				clientRecu.getPrenoms());
-		if (!clients.isPresent()) {
-			LOG.info("Client Enregistré");
-			Client nouveauClient = new Client();
-			nouveauClient.setNom(clientRecu.getNom());
-			nouveauClient.setPrenoms(clientRecu.getPrenoms());
-			this.clientRepository.save(nouveauClient);
-			return ResponseEntity.status(HttpStatus.CREATED).body("Client creer");
-		} else {
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Client possedant deja le meme nom prenoms");
-		}
-
+	@PostMapping
+	public UUID creerClient(@RequestBody @Valid Client clientRecu) {
+		return this.clientService.creerClient(clientRecu);
 	}
 
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<?> validationException(MethodArgumentNotValidException ex) {
+		return ResponseEntity.badRequest().body(ex.getMessage());
+	}
+
+	@ExceptionHandler(value = { EntityExistsException.class })
+	public ResponseEntity<String> ClientPresent(EntityExistsException exception) {
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("client déja en bdd");
+	}
 }
