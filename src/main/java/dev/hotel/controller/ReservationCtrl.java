230ed7992d4ecdb16fda7f51984a4498ee446961
DeJulien <57177431+DeJulien@users.noreply.github.com>
2020-01-27 17:29:23 +0100

    tp 1 maj clean

--- src/main/java/dev/hotel/controller/ReservationCtrl.java
+++ src/main/java/dev/hotel/controller/ReservationCtrl.java
@@ -1,81 +1,51 @@
 package dev.hotel.controller;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import javax.persistence.EntityNotFoundException;
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import dev.hotel.entite.Chambre;
-import dev.hotel.entite.Client;
 import dev.hotel.entite.Reservation;
 import dev.hotel.entite.ReservationJson;
-import dev.hotel.repository.ChambreRepository;
-import dev.hotel.repository.ClientRepository;
-import dev.hotel.repository.ReservationRepository;
+import dev.hotel.service.ReservationService;
 
 @RestController
 public class ReservationCtrl {
-	private static final Logger LOG = LoggerFactory.getLogger(ReservationCtrl.class);
 
-	private ReservationRepository reservationRepository;
-	private ClientRepository clientRepository;
-	private ChambreRepository chambreRepository;
+	private ReservationService reservationService;
 
-	public ReservationCtrl(ReservationRepository reservationRepository, ClientRepository clientRepository,
-			ChambreRepository chambreRepository) {
+	public ReservationCtrl(ReservationService reservationService) {
 		super();
-		this.reservationRepository = reservationRepository;
-		this.clientRepository = clientRepository;
-		this.chambreRepository = chambreRepository;
+		this.reservationService = reservationService;
 	}
 
 	@RequestMapping(method = RequestMethod.POST, path = "reservations")
 	// @ResponseBody
-	public ResponseEntity<String> post(@RequestBody ReservationJson reservationRecu) {
+	public ResponseEntity<Reservation> post(@RequestBody @Valid ReservationJson reservationRecu) {
 
-		Client client = new Client();
-		Reservation reservation = new Reservation();
-		List<Chambre> chambres = new ArrayList<>();
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(this.reservationService.creerReservation(reservationRecu.getDateDebut(),
+						reservationRecu.getDateFin(), reservationRecu.getChambres(), reservationRecu.getClientId()));
 
-		if (clientRepository.findById(reservationRecu.getClientId()).isPresent()) {
+	}
 
-			client = clientRepository.findById(reservationRecu.getClientId()).get();
+	@ExceptionHandler(value = { EntityNotFoundException.class })
+	public ResponseEntity<String> reservationPresent(EntityNotFoundException exception) {
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("erreur " + exception.getMessage());
+	}
 
-			reservation.setClient(client);
-
-		} else {
-
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Client non existant");
-
-		}
-
-		for (UUID chambre : reservationRecu.getChambres()) {
-
-			if (chambreRepository.findById(chambre).isPresent()) {
-				Chambre ch = this.chambreRepository.findById(chambre).get();
-				chambres.add(ch);
-			}
-		}
-
-		if (!chambres.isEmpty()) {
-			LOG.info("");
-			reservation.setDateDebut(reservationRecu.getDateDebut());
-			reservation.setDateFin(reservationRecu.getDateFin());
-			reservation.setChambres(chambres);
-			this.reservationRepository.save(reservation);
-			return ResponseEntity.status(HttpStatus.CREATED).body("Reservation creer");
-		} else {
-
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Chambre non trouve");
-		}
+	@GetMapping
+	public List<Reservation> listerResa() {
+		return this.reservationService.listerReservations();
 
 	}
 
