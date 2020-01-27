650085ebf4644bc1aa173d75a3d9987524d23aa7
DeJulien <57177431+DeJulien@users.noreply.github.com>
2020-01-27 10:35:19 +0100

    Update Startup.java

--- src/main/java/dev/hotel/Startup.java
+++ src/main/java/dev/hotel/Startup.java
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
 
 import dev.hotel.entite.Chambre;
 import dev.hotel.entite.Client;
