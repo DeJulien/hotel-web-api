e76f1d2666b0f9454c4dbd544436e45b70a6c9de
DeJulien <57177431+DeJulien@users.noreply.github.com>
2020-01-27 17:28:15 +0100

    tp 1 maj clean

--- src/main/java/dev/hotel/controller/ChambreCtrl.java
+++ src/main/java/dev/hotel/controller/ChambreCtrl.java
@@ -2,49 +2,26 @@ package dev.hotel.controller;
 
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.hotel.entite.Chambre;
-import dev.hotel.repository.ChambreRepository;
+import dev.hotel.service.ChambreService;
 
 @RestController
 public class ChambreCtrl {
-	private static final Logger LOG = LoggerFactory.getLogger(ChambreCtrl.class);
 
-	private ChambreRepository chambreRepository;
+	private ChambreService chambreService;
 
-	public ChambreCtrl(ChambreRepository chambreRepository) {
+	public ChambreCtrl(ChambreService chambreService) {
 		super();
-		this.chambreRepository = chambreRepository;
+		this.chambreService = chambreService;
 
 	}
 
-	@RequestMapping(method = RequestMethod.GET, path = "chambre")
+	@GetMapping
 	public List<Chambre> retournePlusieursEmployes() {
-		List<Chambre> listChambre = chambreRepository.findAll();
-		return listChambre;
-
-	}
-
-	@RequestMapping(method = RequestMethod.POST, path = "chambre")
-	// @ResponseBody
-	public ResponseEntity<String> post(@RequestBody Chambre chambreRecu) {
-
-		LOG.info("chambre Enregistré");
-		Chambre nouveauChambre = new Chambre();
-		nouveauChambre.setHotel(chambreRecu.getHotel());
-		nouveauChambre.setNumero(chambreRecu.getNumero());
-		nouveauChambre.setSurfaceEnM2(chambreRecu.getSurfaceEnM2());
-
-		this.chambreRepository.save(nouveauChambre);
-		return ResponseEntity.status(HttpStatus.CREATED).body("chambre creer");
+		return this.chambreService.listerChambre();
 
 	}
 
