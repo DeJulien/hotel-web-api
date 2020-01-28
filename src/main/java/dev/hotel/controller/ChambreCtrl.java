b31ddab9ff3396ebd39e0478f2a205fdf0f0ed2f
DeJulien <57177431+DeJulien@users.noreply.github.com>
2020-01-28 14:30:53 +0100

    maj temp

--- src/main/java/dev/hotel/controller/ChambreCtrl.java
+++ src/main/java/dev/hotel/controller/ChambreCtrl.java
@@ -3,12 +3,14 @@ package dev.hotel.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.hotel.entite.Chambre;
 import dev.hotel.service.ChambreService;
 
 @RestController
+@RequestMapping("chambre")
 public class ChambreCtrl {
 
 	private ChambreService chambreService;
